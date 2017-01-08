02477616bbffde0f4113e3460bf2226cdb10364a
Dmitro <Buzalo>
2017-01-08 22:11:08 +0200

    Moved gsm to separate class.

---
+++ main/MyGSM.cpp
@@ -0,0 +1,106 @@
+#include "MyGSM.h"
+#include "Arduino.h"
+
+//#include <SoftwareSerial.h>                 // если программный
+//SoftwareSerial gsm(7, 8); // RX, TX
+
+#define serial Serial                           // если аппаратный в UNO
+//#define serial Serial1                          // если аппаратный в леонардо
+
+MyGSM::MyGSM(int gsmLED, int pinBOOT)
+{
+  _gsmLED = gsmLED;
+  _pinBOOT = pinBOOT;
+}
+
+// Инициализация gsm модуля (включения, настройка)
+void MyGSM::Initialize()
+{
+  serial.begin(9600);                            /// незабываем указать скорость работы UART модема
+  delay(1000);                            
+  digitalWrite(_gsmLED, HIGH);           // на время включаем лед  
+  digitalWrite(_pinBOOT, LOW);           // включаем модем 
+   
+  // нужно дождатся включения модема и соединения с сетью
+  delay(2000);    
+  serial.println("ATE0");                  // выключаем эхо  
+
+  delay(100);
+  serial.println("AT+CLIP=1");  //включаем АОН
+    
+  // настройка смс
+  delay(100);
+  serial.println("AT+CMGF=1");             //режим кодировки СМС - обычный (для англ.)
+  delay(100);
+  serial.println("AT+CSCS=\"GSM\"");       //режим кодировки текста
+  delay(100);
+    
+  while(1)                              // ждем подключение модема к сети
+  {                             
+    serial.println("AT+COPS?");
+    if (serial.find("+COPS: 0")) break;
+    BlinkLED(_gsmLED, 0, 500, 0);        // блымаем светодиодом      
+  }
+
+  serial.println("AT+CLIP=1");             // включаем АОН,
+  
+  //serial.println("Modem OK"); 
+  BlinkLED(_gsmLED, 500, 150, 0);        // блымаем светодиодом 
+  BlinkLED(_gsmLED, 150, 150, 200);      // блымаем светодиодом   
+}
+
+bool MyGSM::Available()
+{
+  return serial.available();
+}
+
+String MyGSM::Read()
+{
+  String val = "";
+  int ch = 0;
+  while (serial.available())                                 //сохраняем входную строку в переменную val
+  {  
+    ch = serial.read();
+    val += char(ch);
+    delay(10);
+  }
+  return val;
+}
+
+//отправка СМС
+void MyGSM::SendSMS(String text, String phone)       //процедура отправки СМС
+{
+  //Serial.println("SMS send started");
+  serial.println("AT+CMGS=\"" + phone + "\"");
+  delay(500);
+  serial.print(text);
+  delay(500);
+  serial.print((char)26);
+  delay(500);
+  //Serial.println("SMS send complete");
+  delay(2000);
+}
+
+// звонок на заданый номер
+void MyGSM::Call(String phone)
+{
+  serial.println("ATD+" + phone + ";");
+  delay(100);
+}
+
+// сброс звонка
+void MyGSM::RejectCall()
+{
+  serial.println("ATH0");
+}
+
+// Блымание gsm светодиодом 
+void MyGSM::BlinkLED(int pinLED, int millisBefore, int millisHIGH, int millisAfter)
+{ 
+  digitalWrite(pinLED, LOW);                          
+  delay(millisBefore);  
+  digitalWrite(pinLED, HIGH);                 // блымаем светодиодом
+  delay(millisHIGH); 
+  digitalWrite(pinLED, LOW);
+  delay(millisAfter);
+};
---
+++ main/MyGSM.h
@@ -0,0 +1,18 @@
+#include "Arduino.h"
+
+class MyGSM
+{
+  public: 
+    MyGSM(int gsmLED, int pinBOOT);
+    void Initialize();
+    bool Available();
+    String Read();
+    void SendSMS(String text, String phone);
+    void Call(String phone);
+    void RejectCall();
+    
+  private:
+    void BlinkLED(int pinLED, int millisBefore, int millisHIGH, int millisAfter);
+    int _gsmLED;
+    int _pinBOOT;                             // нога BOOT или K на модеме   
+};
